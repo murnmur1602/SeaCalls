07c6b23cb11c910b939d61e6b42055df1dc5a6cc
ktochilkin <ktochilkin73@gmail.com>
2026-01-09 09:17:06 +0300

    Initial commit: Sea Calls Next.js project

--- components/sections/GallerySection.tsx
+++ components/sections/GallerySection.tsx
@@ -1,27 +1,37 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+const ResponsiveMasonry = dynamic(
+  () => import("react-responsive-masonry").then((mod) => mod.ResponsiveMasonry),
+  { ssr: false }
+);
+
+const Masonry = dynamic(
+  () => import("react-responsive-masonry").then((mod) => mod.default),
+  { ssr: false }
+);
 
 const photos = [
-  '/images/abb83e2ba131e055681fb6d9d526cc2cfddb2dad.png',
-  '/images/c826716aaac1596962a0c9adb69e01002a32d9b4.png',
-  '/images/f65fc889002f38cbc9b37dedd8e13dbc582f9fee.png',
-  '/images/af21f700576bff6493469285dbee9a0e38057bb1.png',
-  '/images/83b228c04a172651f9b214cab71ff7942ba0d07c.png',
-  '/images/62457fbe49ee52d70677afcb805da99303541530.png',
-  '/images/b5a8eecdebdf1c271602d8880c2a8a68d49c08d6.png',
-  '/images/7798b690540feee8f5b0182440c203dfb4f02a25.png',
-  '/images/925f3a3a87379c80d6f3142ed96b6726b36cf0f8.png',
-  '/images/2d9599f260dfd552885c57189ec2f8fac9a46dc0.png',
-  '/images/1e2bc1bd43b6993b664ab500df653ba43e87b04b.png',
-  '/images/c40fbc63d505fe6449417d9a15f3c9143e82ba1f.png',
-  '/images/38677bdcf18c20c9e0e9456f2a634b25305241af.png',
-  '/images/ac0f2f9ab094708d8bd17bbb686ca42f31f6d811.png',
-  '/images/5e2339ca7508f7783bf3f6b1795903e78cc15730.png',
-  '/images/0a41fbc2bc69d14dec3e2adea3d82aa882bbdf2b.png',
-  '/images/464d1cdf89768ad54b044cd95c1e22551393f48c.png',
-  '/images/55eca80d5f1b5270334a45fc9636ef2306ee4375.png',
+  "/images/abb83e2ba131e055681fb6d9d526cc2cfddb2dad.png",
+  "/images/c826716aaac1596962a0c9adb69e01002a32d9b4.png",
+  "/images/f65fc889002f38cbc9b37dedd8e13dbc582f9fee.png",
+  "/images/af21f700576bff6493469285dbee9a0e38057bb1.png",
+  "/images/83b228c04a172651f9b214cab71ff7942ba0d07c.png",
+  "/images/62457fbe49ee52d70677afcb805da99303541530.png",
+  "/images/b5a8eecdebdf1c271602d8880c2a8a68d49c08d6.png",
+  "/images/7798b690540feee8f5b0182440c203dfb4f02a25.png",
+  "/images/925f3a3a87379c80d6f3142ed96b6726b36cf0f8.png",
+  "/images/2d9599f260dfd552885c57189ec2f8fac9a46dc0.png",
+  "/images/1e2bc1bd43b6993b664ab500df653ba43e87b04b.png",
+  "/images/c40fbc63d505fe6449417d9a15f3c9143e82ba1f.png",
+  "/images/38677bdcf18c20c9e0e9456f2a634b25305241af.png",
+  "/images/ac0f2f9ab094708d8bd17bbb686ca42f31f6d811.png",
+  "/images/5e2339ca7508f7783bf3f6b1795903e78cc15730.png",
+  "/images/0a41fbc2bc69d14dec3e2adea3d82aa882bbdf2b.png",
+  "/images/464d1cdf89768ad54b044cd95c1e22551393f48c.png",
+  "/images/55eca80d5f1b5270334a45fc9636ef2306ee4375.png",
 ];
 
 export default function GallerySection() {
@@ -31,20 +41,25 @@ export default function GallerySection() {
       <div
         className="absolute inset-0 w-full h-full pointer-events-none"
         style={{
-          backgroundImage: 'url(/images/paper-texture.png)',
-          backgroundRepeat: 'repeat',
-          backgroundSize: 'auto',
+          backgroundImage: "url(/images/paper-texture.png)",
+          backgroundRepeat: "repeat",
+          backgroundSize: "auto",
           opacity: 0.4,
-          mixBlendMode: 'multiply',
+          mixBlendMode: "multiply",
         }}
       />
 
       <div className="max-w-7xl mx-auto relative z-10">
         {/* Masonry Grid */}
-        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}>
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}
+        >
           <Masonry gutter="8px">
             {photos.map((photo, index) => (
-              <div key={index} className="relative w-full rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
+              <div
+                key={index}
+                className="relative w-full rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300"
+              >
                 <Image
                   src={photo}
                   alt={`Sailing adventure ${index + 1}`}
